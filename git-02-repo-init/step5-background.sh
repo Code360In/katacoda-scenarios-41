5623715488e04f7c70ca7884b1cf150ef57609c1
Pawel Piwosz <pawel@pawelpiwosz.net>
2021-08-07 00:14:52 +0200

    progress

--- git-02-repo-init/step5-background.sh
+++ git-02-repo-init/step5-background.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+apt-get install -y tree
+cd test-repo
 touch anotherfile && echo "hello again" > anotherfile
 mkdir directory
 touch directory/thirdfile && echo "file in directory" > directory/thirdfile
--- git-02-repo-init/step5.md
+++ git-02-repo-init/step5.md
@@ -3,3 +3,6 @@
 Scenario created some files for you. Let's check them
 
 `tree`{{execute}}
+
+We have two directories, and two files.
+Please notice, one directory is empty.
