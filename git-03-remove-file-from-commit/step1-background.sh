c277de294809f33134d79573f1b1b3920a19a277
Pawel Piwosz <pawel@pawelpiwosz.net>
2021-08-07 16:45:33 +0200

    work

--- git-03-remove-file-from-commit/step1-background.sh
+++ git-03-remove-file-from-commit/step1-background.sh
@@ -3,3 +3,8 @@
 mkdir test
 cd test
 git init
+touch testfile-01 && echo 'first file' > testfile-01
+touch testfile-02 && echo 'second file' > testfile-02
+touch testfile-03 && echo 'fifth file' > testfile-03
+touch testfile-04 && echo 'fourth file' > testfile-04
+git add .
---
+++ git-03-remove-file-from-commit/step1.md
@@ -0,0 +1,5 @@
+# Remove files from commit
+
+Let's check, what we have in stage 
+
+`git status`{{execute}}
